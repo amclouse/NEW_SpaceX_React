d32dfe7538eedfd9a75ce8cfe644fc64826b37c4
Adam Clouse <amclouse96@gmail.com>
2021-02-28 10:16:03 -0500

    home page edit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,7 @@ import "../styles/Home.css";
 const Home = () => {
   return (
     <div className="home-div">
-      <h1 className="home-header">It's SpaceX alright</h1>
-      <img src="../styles/assets/Chloe.JPG" />
+      <h1 className="home-header">Space X home page</h1>
     </div>
   );
 };
